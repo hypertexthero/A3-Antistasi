b9d072e30d3260c68781ceb0b5c2acddb830f2dd
Simon Griffee <simon@hypertexthero.com>
2020-06-09 13:12:55 -0400

    Update CfgIdentities.hpp
    Using Max_Women with permission.

--- A3-Antistasi/MissionDescription/CfgIdentities.hpp
+++ A3-Antistasi/MissionDescription/CfgIdentities.hpp
@@ -1,156 +1,156 @@
-class CfgIdentities
+class CfgIdentities // Using Max_Women with permission - https://steamcommunity.com/workshop/filedetails/?id=1451755886
 {
   class protagonista
   {
-    name = "Stavros";
-    nameSound = "Stavrou";
-    face="GreekHead_A3_01";
+    name = "Kassandra";
+    nameSound = "Kassandra";
+    face="max_female1";
     glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
+    speaker="Male01ENGB";
+    pitch=1.4;
   };
   class friendlyX
   {
-    name = "Petros";
-    nameSound = "Petros";
-    face="GreekHead_A3_01";
+    name = "Joan";
+    nameSound = "Joan";
+    face="max_female2";
     glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
+    speaker="Male02ENGB";
+    pitch=1.4;
   };
-  class Anthis
+  class Jessica
   {
-    name = "Anthis";
-    nameSound = "Anthis";
-    face="GreekHead_A3_02";
+    name = "Jessica";
+    nameSound = "Jessica";
+    face="max_female3";
     glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
+    speaker="Male03ENGB";
+    pitch=1.5;
   };
-  class Costa
+  class Adi
   {
-    name = "Costa";
-    nameSound = "Costa";
-    face="GreekHead_A3_03";
+    name = "Adi";
+    nameSound = "Adi";
+    face="max_female4";
     glasses="None";
-    speaker="Male02GRE";
-    pitch=1.1;
+    speaker="Male04ENGB";
+    pitch=1.5;
   };
-  class Dimitirou
+  class Jillian
   {
-    name = "Dimitirou";
-    nameSound = "Dimitirou";
-    face="GreekHead_A3_04";
+    name = "Jillian";
+    nameSound = "Jillian";
+    face="max_female5";
     glasses="None";
-    speaker="Male03GRE";
-    pitch=1.1;
+    speaker="Male05ENGB";
+    pitch=1.3;
   };
-  class Elias
+  class Ensaf
   {
-    name = "Elias";
-    nameSound = "Elias";
-    face="GreekHead_A3_05";
+    name = "Ensaf";
+    nameSound = "Ensaf";
+    face="max_female6";
+    glasses="None";
+    speaker="Male01ENGFRE";
+    pitch=1.5;
+  };
+  class Yua
+  {
+    name = "Yua";
+    nameSound = "Yua";
+    face="max_female7";
+    glasses="None";
+    speaker="Male11ENG";
+    pitch=1.7;
+  };
+  class Chen
+  {
+    name = "Chen";
+    nameSound = "Chen";
+    face="max_female8";
+    glasses="None";
+    speaker="Male01CHI";
+    pitch=1.6;
+  };
+  class Emelyne
+  {
+    name = "Emelyne";
+    nameSound = "Emelyne";
+    face="max_female9";
+    glasses="None";
+    speaker="Male01FRE";
+    pitch=1.4;
+  };
+  class Karenina
+  {
+    name = "Karenina";
+    nameSound = "Karenina";
+    face="max_female10";
+    glasses="None";
+    speaker="Male01RUS";
+    pitch=1.5;
+  };
+  class Anna
+  {
+    name = "Anna";
+    nameSound = "Anna";
+    face="max_female11";
+    glasses="None";
+    speaker="Male02RUS";
+    pitch=1.3;
+  };
+  class Amelia
+  {
+    name = "Amelia";
+    nameSound = "Amelia";
+    face="max_female12";
+    glasses="None";
+    speaker="Male01POL";
+    pitch=1.6;
+  };
+  class Abebi
+  {
+    name = "Abebi";
+    nameSound = "Abebi";
+    face="max_female13";
+    glasses="None";
+    speaker="Male10ENG";
+    pitch=1.4;
+  };
+  class Anais
+  {
+    name = "Anais";
+    nameSound = "Anais";
+    face="max_female14";
+    glasses="None";
+    speaker="Male03FRE";
+    pitch=1.4;
+  };
+  class Ursula
+  {
+    name = "Ursula";
+    nameSound = "Ursula";
+    face="max_female15";
+    glasses="None";
+    speaker="Male12ENG";
+    pitch=1.5;
+  };
+  class Lara
+  {
+    name = "Lara";
+    nameSound = "Lara";
+    face="max_female16";
     glasses="None";
     speaker="Male04GRE";
-    pitch=1.1;
+    pitch=1.5;
   };
-  class Gekas
+  class Jennifer
   {
-    name = "Gekas";
-    nameSound = "Gekas";
-    face="GreekHead_A3_06";
+    name = "Jennifer";
+    nameSound = "Jennifer";
+    face="max_female17";
     glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
-  };
-  class Kouris
-  {
-    name = "Kouris";
-    nameSound = "Kouris";
-    face="GreekHead_A3_07";
-    glasses="None";
-    speaker="Male02GRE";
-    pitch=1.1;
-  };
-  class Leventis
-  {
-    name = "Leventis";
-    nameSound = "Leventis";
-    face="GreekHead_A3_08";
-    glasses="None";
-    speaker="Male03GRE";
-    pitch=1.1;
-  };
-  class Markos
-  {
-    name = "Markos";
-    nameSound = "Markos";
-    face="GreekHead_A3_09";
-    glasses="None";
-    speaker="Male04GRE";
-    pitch=1.1;
-  };
-  class Nikas
-  {
-    name = "Nikas";
-    nameSound = "Nikas";
-    face="WhiteHead_02";
-    glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
-  };
-  class Nicolo
-  {
-    name = "Nicolo";
-    nameSound = "Nicolo";
-    face="WhiteHead_03";
-    glasses="None";
-    speaker="Male02GRE";
-    pitch=1.1;
-  };
-  class Panas
-  {
-    name = "Panas";
-    nameSound = "Panas";
-    face="WhiteHead_04";
-    glasses="None";
-    speaker="Male03GRE";
-    pitch=1.1;
-  };
-  class Rosi
-  {
-    name = "Rosi";
-    nameSound = "Rosi";
-    face="WhiteHead_05";
-    glasses="None";
-    speaker="Male04GRE";
-    pitch=1.1;
-  };
-  class Samaras
-  {
-    name = "Samaras";
-    nameSound = "Samaras";
-    face="WhiteHead_06";
-    glasses="None";
-    speaker="Male01GRE";
-    pitch=1.1;
-  };
-  class Thanos
-  {
-    name = "Thanos";
-    nameSound = "Thanos";
-    face="WhiteHead_07";
-    glasses="None";
-    speaker="Male02GRE";
-    pitch=1.1;
-  };
-  class Vega
-  {
-    name = "Vega";
-    nameSound = "Vega";
-    face="WhiteHead_08";
-    glasses="None";
-    speaker="Male03GRE";
-    pitch=1.1;
+    speaker="Male02ENGFRE";
+    pitch=1.4;
   };
 };
